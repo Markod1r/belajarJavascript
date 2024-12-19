b5f51812588822214c936913238b90966349a3b9
marcoismakalew <marcoismakalew@gmail.com>
2024-12-19 19:59:30 +0800

    belajar looping

---
+++ scriptexercise.js
@@ -0,0 +1,10 @@
+var noAngkot;
+var jumlahAngkot = 10;
+
+for(noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
+    if (noAngkot <= 6) {
+        console.log ('Angkot No. ' + noAngkot + ' telah beroperasi.');
+    } else {
+        console.log ('Angkot No. ' + noAngkot + ' tidak beroperasi.');
+    }
+}
